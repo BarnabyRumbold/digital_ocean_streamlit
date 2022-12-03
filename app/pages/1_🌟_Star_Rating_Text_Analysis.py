c13dc99f7ddfef1e1379f981eb5a3ca150101a40
Johanna <85117688+JohannaWS@users.noreply.github.com>
2022-12-03 11:46:10 +0100

    commit

--- app/pages/1_🌟_Star_Rating_Text_Analysis.py
+++ app/pages/1_🌟_Star_Rating_Text_Analysis.py
@@ -10,12 +10,12 @@ from nltk.tokenize import sent_tokenize
 from nltk.tokenize import RegexpTokenizer
 from string import punctuation as punc
 from nltk.stem import WordNetLemmatizer
-sw = nltk.download('stopwords')
-# from nltk.corpus import stopwords as sw
+
+from nltk.corpus import stopwords as sw
 import plotly.graph_objs as go
 import re
-
-
+x = sw.download('stopwords')
+st.write(type(w))
 ### SETUP ###
 loc_path=""# for local testing
 her_path=""
